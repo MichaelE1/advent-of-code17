15954087138a7280fdbd77c79dd3974b7c1c5879
Michael <mickel13@gmail.com>
2017-12-10 11:01:34 +1000

    completed 7a

--- Day7/7a.js
+++ Day7/7a.js
@@ -4,13 +4,22 @@ let input = fs.readFileSync('./input.txt', 'utf8');
 
 let parents = input.split('\n').map(line => line.substr(0, line.indexOf(' ')));
 
-let children = input.split('\n');// map(line => line.substr(line.indexOf(' ')[1]));
+let children = input.split('\n');
 
+// Get only the children nodes
 for (let i = children.length - 1; i >= 0; i--) {
-    console.log(children[i]);
     if (!children[i].includes(',')) {
         children.splice(i, 1);
+    } else {
+        let pattern = /[a-z]+\s\([0-9]+\)\s->\s/;
+        children[i] = children[i].replace(pattern,"").replace(/\s/g, "").split(',');
     }
 }
 
-console.log(children);
+// Turn 2D child array into 1D
+children = [].concat(...children);
+
+// Find root by calculating the set difference
+let root = parents.filter(x => children.indexOf(x) < 0);
+
+console.log(root);
